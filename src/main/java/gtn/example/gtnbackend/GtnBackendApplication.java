b21a0c3c538f43ffadf2b7d2151e93caf287bce6
StahlAttila <stattilasd@gmail.com>
2020-04-26 12:08:45 +0200

    init commit

---
+++ src/main/java/gtn/example/gtnbackend/GtnBackendApplication.java
@@ -0,0 +1,13 @@
+package gtn.example.gtnbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtnBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GtnBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/gtn/example/gtnbackend/GtnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gtn.example.gtnbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtnBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
